20df635bcbc3b5f1941872e924b817b3c27b3e31
Shadow <129654147+shadow-anderson@users.noreply.github.com>
2025-06-20 22:00:46 +0530

    Update amadeusService.js

--- client/src/services/amadeusService.js
+++ client/src/services/amadeusService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://travel-9qtu.onrender.com/';
+const API_BASE_URL = 'https://travel-9qtu.onrender.com';
 
 export const fetchAirportsByCountry = async (country) => {
   try {
